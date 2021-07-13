fd76e67afd86c2340f200288240ce88dd7c368cc
Paul Okeke <pokeke@teamapt.com>
2021-07-13 08:41:39 +0100

    motion detection in progress

--- lib/app/onboarding/views/new/liveliness2_view.dart
+++ lib/app/onboarding/views/new/liveliness2_view.dart
@@ -13,19 +13,45 @@ class Liveliness2 extends StatefulWidget {
 class _Liveliness2 extends State<Liveliness2> {
   static const platform = const MethodChannel('moniepoint.flutter.dev/liveliness');
 
+  int? _cameraId;
+  Future<dynamic>? cameraFuture;
+
   @override
   void initState() {
-    // TODO: implement initState
-    super.initState();
     _startCamera();
+    super.initState();
+
+    cameraFuture?.whenComplete(() async {
+      print("Initializing liveliness from flutter");
+      await platform.invokeMethod("initialize");
+    });
   }
 
   void _startCamera() async {
-    await platform.invokeMethod("analyzeImage");
+    cameraFuture = platform.invokeMethod("create");
+    final result = await cameraFuture;
+    final data = result as Map<dynamic, dynamic>;
+    _cameraId = data["cameraId"];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    
+    return FutureBuilder(
+        future: cameraFuture,
+        builder: (mContext, AsyncSnapshot<dynamic> v) {
+          if(v.connectionState != ConnectionState.done) return Container();
+          print("Camera ID is $_cameraId");
+          return Container(
+            child: SizedBox(
+              child: AspectRatio(
+                aspectRatio: 4/3,
+                child: Texture(textureId: _cameraId!),
+              )
+            ),
+          );
+        }
+    );
   }
 }

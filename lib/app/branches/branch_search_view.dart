bf2f097e007c2641de953bd279bfbf30fa336955
Obinna Igwe <obinna.igwe@teamapt.com>
2021-07-27 08:11:49 +0100

    Redesign search page screen

--- lib/app/branches/branch_search_view.dart
+++ lib/app/branches/branch_search_view.dart
@@ -22,7 +22,7 @@ class _BranchSearchScreen extends State<BranchSearchScreen> {
   bool _isLoading = false;
 
   void _onSearchFieldChange(BranchViewModel viewModel, String text) {
-    if(text.isEmpty) {
+    if (text.isEmpty) {
       _searchController.text = "";
     }
     if (text.isNotEmpty && text.length <= 2) return;
@@ -45,30 +45,34 @@ class _BranchSearchScreen extends State<BranchSearchScreen> {
     });
   }
 
-
   Widget _emptyView() {
     return Container(
       alignment: Alignment.center,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(height: 15,),
-          SvgPicture.asset('res/drawables/ic_branch_info.svg', width: 48, height: 48, color: Colors.colorFaded,),
-          SizedBox(height: 16,),
+          SizedBox(
+            height: 15,
+          ),
+          SvgPicture.asset(
+            'res/drawables/ic_branch_info.svg',
+            width: 48,
+            height: 48,
+            color: Colors.colorFaded,
+          ),
+          SizedBox(
+            height: 16,
+          ),
           Flexible(
               child: Padding(
-                padding: EdgeInsets.only(left: 24, right: 24),
-                child: Text(
-                    'Enter a moniepoint branch name to search.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600
-                    )
-                ),
-              )
-          )
+            padding: EdgeInsets.only(left: 24, right: 24),
+            child: Text('Enter a moniepoint branch name to search.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600)),
+          ))
         ],
       ),
     );
@@ -81,44 +85,63 @@ class _BranchSearchScreen extends State<BranchSearchScreen> {
     return Scaffold(
       backgroundColor: Colors.backgroundWhite,
       body: Container(
-        padding: EdgeInsets.only(top: 64),
+        padding: EdgeInsets.only(top: 38),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: [
-            Flexible(child: Container(
-              margin: EdgeInsets.only(left: 16, right: 16),
+            Flexible(
+                child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
               // padding: EdgeInsets.only(top: 19, bottom: 19),
               child: TextFormField(
                   controller: _searchController,
                   onChanged: (v) => _onSearchFieldChange(viewModel, v),
                   textAlignVertical: TextAlignVertical.center,
                   decoration: InputDecoration(
-                      hintText: "Search by Location",
-                      contentPadding: EdgeInsets.only(top: 19,bottom: 19),
+                      hintText: "Search by branch name",
+                      hintStyle: TextStyle(
+                          color: Color(0xFF4A4A4A).withOpacity(0.2934)),
+                      contentPadding: EdgeInsets.only(top: 19, bottom: 19),
                       prefixIcon: GestureDetector(
                         onTap: () {
-                          if(_searchController.text.isNotEmpty) _onSearchFieldChange(viewModel, "");
-                          else Navigator.of(context).pop();
+                          if (_searchController.text.isNotEmpty)
+                            _onSearchFieldChange(viewModel, "");
+                          else
+                            Navigator.of(context).pop();
                         },
                         child: Padding(
-                          padding: EdgeInsets.only(top:12,right: 16, bottom: 12, left: 16),
-                          child: Icon(CustomFont.backArrow, size: 20, color: Colors.colorFaded,),
+                          padding: EdgeInsets.only(
+                              top: 12, right: 16, bottom: 12, left: 16),
+                          child: Icon(
+                            CustomFont.backArrow,
+                            size: 20,
+                            color: Color(0xFF9DA1AB),
+                          ),
                         ),
                       ),
                       suffixIcon: _isLoading
                           ? Padding(
                               padding: EdgeInsets.only(right: 16),
-                              child: SizedBox(height:10, width:10,child: SpinKitThreeBounce(size: 20.0, color: Colors.primaryColor.withOpacity(0.8)),),
+                              child: SizedBox(
+                                height: 10,
+                                width: 10,
+                                child: SpinKitThreeBounce(
+                                    size: 20.0,
+                                    color:
+                                        Colors.primaryColor.withOpacity(0.8)),
+                              ),
                             )
                           : null,
                       isCollapsed: true,
-                      disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent)),
-                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent)),
-                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent)),
-                      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent))
-                  )
-              ),
+                      disabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent)),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent)))),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(6),
@@ -127,38 +150,71 @@ class _BranchSearchScreen extends State<BranchSearchScreen> {
                         offset: Offset(0, 1),
                         color: Colors.grey.withOpacity(0.2),
                         blurRadius: 6,
-                        spreadRadius: 2
-                    )
+                        spreadRadius: 2)
                   ]),
             )),
-            SizedBox(height: 20),
+            SizedBox(height: 50),
+            StreamBuilder(
+                stream: viewModel.searchResultStream,
+                builder:
+                    (context, AsyncSnapshot<Resource<List<BranchInfo>>> a) {
+                  if (!a.hasData) return Container();
+                  // if ((a.hasData && a.data is Success) &&
+                  //     a.data?.data?.isEmpty == true) {
+                  //   return _emptyView();
+                  // }
+
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 22),
+                        child: Text(
+                          "SEARCH RESULTS",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.textColorBlack,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ],
+                  );
+                }),
+            SizedBox(height: 5),
             Expanded(
-                flex: 0,
-                child: Divider(color: Colors.colorFaded.withOpacity(0.5), height: 1)
-            ),
-            SizedBox(height: 12),
-            Expanded(
-                child: StreamBuilder(
-                    stream: viewModel.searchResultStream,
-                    builder: (context, AsyncSnapshot<Resource<List<BranchInfo>>> a) {
-                      if (!a.hasData) return Container();
-                      if((a.hasData && a.data is Success) && a.data?.data?.isEmpty == true) {
-                        return _emptyView();
-                      }
-                      return ListView.separated(
-                        padding: EdgeInsets.zero,
-                        shrinkWrap: true,
-                        itemCount: a.data?.data?.length ?? 0,
-                        separatorBuilder: (context, index) => SizedBox(height: 16,),
-                        itemBuilder: (context, index) {
-                          return _BranchListItem(a.data!.data![index], index, (item, itemIndex) {
-                              Future.delayed(Duration(milliseconds: 180), (){
-                                Navigator.of(context).pop(item);
-                              });
+              child: StreamBuilder(
+                  stream: viewModel.searchResultStream,
+                  builder:
+                      (context, AsyncSnapshot<Resource<List<BranchInfo>>> a) {
+                    if (!a.hasData) return Container();
+                    if ((a.hasData && a.data is Success) &&
+                        a.data?.data?.isEmpty == true) {
+                      return _emptyView();
+                    }
+
+                    return ListView.separated(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      itemCount: a.data?.data?.length ?? 0,
+                      separatorBuilder: (context, index) => Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          children: [
+                            // SizedBox(height: 15),
+                            Divider(height: 1),
+                          ],
+                        ),
+                      ),
+                      itemBuilder: (context, index) {
+                        return _BranchListItem(a.data!.data![index], index,
+                            (item, itemIndex) {
+                          Future.delayed(Duration(milliseconds: 180), () {
+                            Navigator.of(context).pop(item);
                           });
-                        },
-                      );
-                    })
+                        });
+                      },
+                    );
+                  }),
             )
           ],
         ),
@@ -174,12 +230,11 @@ class _BranchSearchScreen extends State<BranchSearchScreen> {
 }
 
 class _BranchListItem extends Container {
-
   final BranchInfo _branchInfo;
   final int position;
   final OnItemClickListener<BranchInfo, int> _onItemClickListener;
 
-  _BranchListItem(this._branchInfo, this.position,  this._onItemClickListener);
+  _BranchListItem(this._branchInfo, this.position, this._onItemClickListener);
 
   Widget initialContainer() {
     return Container(
@@ -187,46 +242,45 @@ class _BranchListItem extends Container {
       width: 42,
       padding: EdgeInsets.all(8),
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.primaryColor.withOpacity(0.1)
-      ),
-      child: SvgPicture.asset('res/drawables/ic_location.svg'),
-    );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      shadowColor: Colors.grey.withOpacity(0.2),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(9))
-      ),
-      margin: EdgeInsets.only(left: 16, right: 16),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          borderRadius: BorderRadius.circular(9),
-          onTap: () => _onItemClickListener.call(_branchInfo, 0),
-          child: Container(
-            padding: EdgeInsets.only(top: 10, bottom: 10, left: 12, right: 12),
-            child: Row(
-              children: [
-                initialContainer(),
-                SizedBox(width: 20),
-                Expanded(child: Text(
-                  _branchInfo.name ?? "",
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 16, color: Colors.textColorBlack,),)),
-                SvgPicture.asset('res/drawables/ic_branch_link.svg'),
-              ],
-            ),
-          ),
+          shape: BoxShape.circle, color: Color(0xFF9DA1AB).withOpacity(0.1)),
+      child: Center(
+        child: SvgPicture.asset(
+          'res/drawables/ic_location_2.svg',
+          height: 20,
+          width: 20,
         ),
       ),
     );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(9),
+        onTap: () => _onItemClickListener.call(_branchInfo, 0),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            children: [
+              initialContainer(),
+              SizedBox(width: 20),
+              Expanded(
+                  child: Text(
+                _branchInfo.name ?? "",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.textColorBlack,
+                ),
+              )),
+              SvgPicture.asset('res/drawables/ic_branch_link_2.svg'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
